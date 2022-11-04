ea46e11fdfd86344c046ddb5c0729a0ff1d726d1
NicolaeMunt <114097090+NicolaeMunt@users.noreply.github.com>
2022-11-04 20:22:50 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+function Dimon() {
+    console.log("Dimon ii sobaka crivonogaia")
+}
+
+Dimon()
+
+
+let loh = {
+    name:"Dimon" ,
+    age: 18  ,
+    race:'sobaka' ,
+    color:"dirty-blonde" ,
+    iq:"tupoi" ,
+    makerace(){
+        console.log(this.race)
+    }
+
+
+}
+loh.born="2004"
+console.log(loh.iq)
+console.log(loh.sound="hehehehe")
+loh.makerace()
+
